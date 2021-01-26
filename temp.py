8cd5f2bb350f6d641ab5b699cf79023cd694199b
locky192 <locky192@gmail.com>
2021-01-26 17:17:13 +1000

    tidy up

--- temp.py
+++ temp.py
@@ -25,9 +25,6 @@ def getTempData(station):
 
     patternc = "[0-9]*\.?[0-9]"
 
-
-    #print soup
-
     result = re.findall(patterna, str(soup))
 
     result3 = re.findall(patternb, str(result))
@@ -36,18 +33,8 @@ def getTempData(station):
 
     result4 = re.findall(patternc, str(result2))
 
-    #print result4
-
-    ##years1 = re.findall(">([0-9][0-9][0-9][0-9])<", str(result3))
-    ##
-    ##temps = re.findall("[0-9]*\.[0-9]", str(result3))
-    ##
-    ##for i in range(0, len(years1)): 
-    ##    years1[i] = int(years1[i]) 
-    ##
     for i in range(0, len(result4)): 
         result4[i] = float(result4[i]) 
-
 
     #-------------------------
 
@@ -57,12 +44,9 @@ def getTempData(station):
 
     x = 1850.0
 
-    while x < currentYear-1:
+    while x < currentYear:
         years.append(x)
         x = x + 1
-
-
-    #print(result4)
 
     y = 0
     dp = -1
@@ -86,38 +70,15 @@ def getTempData(station):
                 temps.append("")
             dp = -1
             y = i
-        
-        
-
-
-
-    ##y = 0
-    ##j = 1
-    ##
-    ##for i in range(0, len(years)):
-    ##    if (years[i] == result4[y]):
-    ##        years1.append(years[i])
-    ##        while (result4[y+j] != years[i] + 1):
-    ##            temps.append(result4[y+j])
-    ##            j = j + 1
-    ##        y = j
-    ##    else:
-    ##        years1.append(years[i])
-    ##        temps.append("")
-    ##    j = 1
 
     temps2 = []
 
     y = 0
 
-    #print years
-    #print years1
-
     if len(years1) == 0:
         return
 
     for i in range(0, len(years)):
-        #print years[i] , years1[y]
         if years[i] != years1[y]:
             temps2.append("")
         else:
@@ -126,7 +87,6 @@ def getTempData(station):
                 y = 0
             else:
                 y = y + 1
-
 
     x = 0
     csvfile = 'WeatherData.csv'
@@ -139,30 +99,21 @@ def getTempData(station):
             writer.writerow(row + [temps2[x]])
             x = x + 1
             
-    os.remove(csvfile) # not needed on unix
+    os.remove(csvfile)
     os.rename('new_'+csvfile, csvfile)
-
 
 #----------------------------------------
 
 currentYear = datetime.datetime.now().year
 
-try:
-    os.remove('file.txt')
-except:
-    None
-
-urllib.request.urlretrieve('ftp://ftp.bom.gov.au/anon2/home/ncc/metadata/lists_by_element/alpha/alphaAUS_122.txt', 'file.txt')
-
-with open('file.txt', 'r') as file:
-    data = file.read()
+myurl = 'ftp://ftp.bom.gov.au/anon2/home/ncc/metadata/lists_by_element/alpha/alphaAUS_122.txt'
+myreq = urllib.request.urlopen(myurl)    
+data = myreq.read()
 
 pattern = "[ |\n] ([0-9][0-9][0-9][0-9]?[0-9]?[0-9])"
-
 patternAWS = "(Y)\n|(N)\n| ( )\n"
 
 result = re.findall(pattern, str(data))
-
 AWS = re.findall(patternAWS, str(data))
 
 AWS1 = []
@@ -179,7 +130,7 @@ column1 = []
 
 x = 1850.0
 
-while x < currentYear-1:
+while x < currentYear:
     column1.append(x)
     x = x + 1
 
@@ -192,14 +143,14 @@ with open('WeatherData.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['Year'])
     for val in column1:
-        #print(val)
         writer.writerow([val])
 
-x = 0
+x = 1
 
 getAWS = False
 
 for i in result:
+    print("Downloading Data for Station", x, "of", len(result))
     #if AWS1[x] == getAWS:
     getTempData(i)
-    #x = x + 1
+    x = x + 1
